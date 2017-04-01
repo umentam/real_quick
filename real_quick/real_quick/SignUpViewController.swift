27b0004ea12a4882c3f8f92e12c04e19b3fc8aad
Michael Umenta <michael.umenta@vanderbilt.edu>
2017-04-01 03:33:39 -0500

    got sign up working

--- real_quick/real_quick/SignUpViewController.swift
+++ real_quick/real_quick/SignUpViewController.swift
@@ -7,17 +7,44 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
+    
+    var ref:FIRDatabaseReference!
+    
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        ref = FIRDatabase.database().reference()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signUp(_ sender: AnyObject) {
+        
+        var nameString = name.text
+        var emailString = email.text
+        var passwordString = password.text
+        
+        FIRAuth.auth()?.createUser(withEmail: emailString!, password: passwordString!) { (user, error) in
+            if error != nil {
+                print(error)
+                return
+            }else{
+                self.ref.child("users").child(user!.uid).setValue(["username": nameString!, "email-address": emailString!])
+                return
+            }
+        }
+        
+        
     }
     
     
