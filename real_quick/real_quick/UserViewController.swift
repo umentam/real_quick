a22d1e0bd0a076d57e290c5fcca524f45979416b
Michael Umenta <michael.umenta@vanderbilt.edu>
2017-04-01 08:56:35 -0500

    added changes to userviewcontroller

--- real_quick/real_quick/UserViewController.swift
+++ real_quick/real_quick/UserViewController.swift
@@ -35,6 +35,8 @@ class UserViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController?.navigationBar.topItem?.title = "Choose your Groceries"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor(red: 2.0/255.0, green: 208.0/255.0, blue: 172.0/255.0, alpha: 1.0),NSFontAttributeName:UIFont.systemFont(ofSize: 25, weight: UIFontWeightLight)]
     }
     
     override func didReceiveMemoryWarning() {
